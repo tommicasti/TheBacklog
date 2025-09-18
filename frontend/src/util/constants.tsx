df5d1cecd8f163d8f338fe13bed0e566ca39ca27
Rosario Grossi <rosario.grossi@peoplefirst.ws>
2025-09-18 15:21:40 +0200

    change constant api

--- frontend/src/util/constants.tsx
+++ frontend/src/util/constants.tsx
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://localhost:5141/api/controller';
+export const BASE_API_URL = 'http://localhost:5141/api/';
 
-export const LOGIN_API_URL = `${BASE_API_URL}/login`;
-export const REGISTER_API_URL = `${BASE_API_URL}/register`;
+export const LOGIN_API_URL = `${BASE_API_URL}auth/login`;
+export const REGISTER_API_URL = `${BASE_API_URL}auth/register`;
